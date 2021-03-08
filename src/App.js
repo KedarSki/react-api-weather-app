867f77c88b7574d345bc7afed4b25eda5120efd1
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2021-03-09 00:22:29 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Jumbotron, Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello Weather</h1>
-      </header>
+      <Jumbotron className={"jumbo"}>
+        <h1>Hello, world!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
     </div>
   );
 }
