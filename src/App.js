b6e9ec2b524075999c0191fece418349c9c6695d
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2021-03-09 19:49:42 +0100

    Background image, input added.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Jumbotron, Button } from "react-bootstrap";
+import { Jumbotron, Form, Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <Jumbotron className={"jumbo"}>
-        <h1>Hello, world!</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
-        </p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
-      </Jumbotron>
+      <Container className={"cont"}>
+        <Jumbotron className={"jumbo"}>
+          <h1>Weather Application</h1>
+          <p>Check the weather anywhere in the world!</p>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Control type="email" placeholder="Enter City and Country" />
+            </Form.Group>
+          </Form>
+        </Jumbotron>
+      </Container>
     </div>
   );
 }
