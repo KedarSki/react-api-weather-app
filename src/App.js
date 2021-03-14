8767985c18d091c765115c32dc79b93de33c7fb6
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2021-03-14 19:18:36 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Jumbotron, Form, Container, Button } from "react-bootstrap";
-import { WiDaySunny } from "weather-icons-react";
+import { getWeatherData } from "./data/WeatherApi";
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState("Lodz");
+  const [loading, setLoading] = useState(false);
+
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const data = await getWeatherData(city);
+      setWeatherData(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error.message);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="App">
       <Container className={"cont"}>
         <Jumbotron className={"jumbo"}>
           <h1>Weather Application</h1>
           <p>Check the weather anywhere in the world!</p>
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Control type="email" placeholder="Enter City and Country" />
-            </Form.Group>
-            <Button variant="info">Search Weather</Button>{" "}
-          </Form>
-          <i className={"fa fa-sun"}></i>
-          <h3>Sunny</h3>
-          <h2 className={"temperature"}>Temperature</h2>
-          <h1>25&deg;C</h1>
-          <h3 className={"location"}>
-            <i className={"fa fa-street-view"}> </i>
-            Lodz | Poland
-          </h3>
-          <h6 className={"temperature-range"}>
-            Min: 25&deg;C || Max: 28&deg;C || Humidity: 10%
-          </h6>
+          <inputGroup>
+            <Form.Control
+              type="text"
+              value={city}
+              onChange={(event) => setCity(event.target.value)}
+              placeholder="Enter City and Country"
+            />
+          </inputGroup>
+          <Button variant="info" onClick={() => getData()}>
+            Search Weather
+          </Button>
+
+          {weatherData !== null
+            ? ((
+                <img
+                  src={
+                    "https://openweathermap.org/img/w/${weatherData.weather[0].icon}"
+                  }
+                  alt="imgicon"
+                >
+                  {" "}
+                </img>
+              ),
+              (<h3>{weatherData.weather[0].main}</h3>),
+              (<h2 className={"temperature"}>Temperature</h2>),
+              (
+                <h1>
+                  {parseFloat(weatherData.main.temp - 273.15).toFixed(1)}
+                  &deg;C
+                </h1>
+              ),
+              (
+                <h3 className={"location"}>
+                  <i className={"fa fa-street-view"}> </i>
+                  {weatherData.name()} | {weatherData.sys.country}
+                </h3>
+              ),
+              (
+                <h6 className={"temperature-range"}>
+                  Min:
+                  {parseFloat(weatherData.main.temp_min - 273.15).toFixed(1)}
+                  &deg;C || Max:
+                  {parseFloat(weatherData.main.temp_max - 273.15).toFixed(1)}
+                  &deg;C || Humidity: {weatherData.main.humidity}%
+                </h6>
+              ))
+            : null}
         </Jumbotron>
       </Container>
     </div>
--- src/data/WeatherApi.js
+++ src/data/WeatherApi.js
@@ -1,10 +1,13 @@
 import React from "react";
 import axios from "axios";
 
+const baseURL = "http://api.openweathermap.org/data/2.5/weather?";
+const apiKey = "f86740c4006c0c8996a4492e4e8f2df9";
 
-
-export const getWeatherData = async () => {
+export const getWeatherData = async (cityName) => {
   try {
+    const { data } = await axios.get(baseURL + "q=${cityName}&appid=${apiKey}");
+    return data;
   } catch (error) {
     throw error;
   }
