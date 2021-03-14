4d48698df806fd313734a3392744ac7e35a5ecb8
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2021-03-14 21:58:31 +0100

    All weather Api data is working. Application can be compiled.

--- src/data/WeatherApi.js
+++ src/data/WeatherApi.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 const baseURL = "http://api.openweathermap.org/data/2.5/weather?";
-const apiKey = "3b278956367a63c02624975120217c4b";
+const apiKey = "f86740c4006c0c8996a4492e4e8f2df9";
 
 export const getWeatherData = async (cityName) => {
   try {
